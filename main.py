cdbc46bdc9d81bd282e1b2b38d1e8c55ea3c4a60
fatimatasowgueye <mmegueye902@gmail.com>
2023-09-19 18:59:49 +0000

    Créer l'arborescence de dossiers

---
+++ main.py
@@ -0,0 +1,27 @@
+# main.py
+
+import os
+
+def create_directory_structure():
+    try:
+        os.mkdir("data")
+        os.mkdir("data/cleaned")
+        os.mkdir("data/raw")
+        os.mkdir("docs")
+        os.mkdir("models")
+        os.mkdir("notebooks")
+        os.mkdir("reports")
+        os.mkdir("src")
+    except FileExistsError:
+        print("Les dossiers existent déjà.")
+
+def commit_changes(message):
+    os.system(f'git add . && git commit -m "{message}"')
+
+def main():
+    # Crée l'arborescence de dossiers
+    create_directory_structure()
+    commit_changes("Créer l'arborescence de dossiers")
+
+if __name__ == "__main__":
+    main()
